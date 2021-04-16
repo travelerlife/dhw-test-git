511224030b879d73355bff5b3f938dae2bafd857
travelerlife <travelerlife@users.noreply.github.com>
2021-04-17 01:18:12 +0800

    A提交。

---
+++ src/main/java/com/dhw/test/git/ForCommitConflict.java
@@ -0,0 +1,17 @@
+package com.dhw.test.git;
+
+/**
+ * 用于解决冲突的演示。
+ * @author haiwei.deng
+ * @time 2021年4月17日 上午1:13:43
+ */
+public class ForCommitConflict {
+	
+	public void methodA() {
+		System.out.println("local machine");
+		System.out.println("local machine");
+		System.out.println("local machine");
+		System.out.println("local machine");
+	}
+	
+}
