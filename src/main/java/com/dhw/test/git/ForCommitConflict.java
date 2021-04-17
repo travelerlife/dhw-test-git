51486a951d870f929c327c256b3d93d51205de25
travelerlife <travelerlife@users.noreply.github.com>
2021-04-17 23:56:37 +0800

    A

--- src/main/java/com/dhw/test/git/ForCommitConflict.java
+++ src/main/java/com/dhw/test/git/ForCommitConflict.java
@@ -22,7 +22,7 @@ public class ForCommitConflict {
 		System.out.println("测试强推覆盖，B提交");
 		System.out.println("测试强推覆盖，A要冲突了");
 		
-		System.out.println("测试强推覆盖，AAAA");
+		System.out.println("测试强推覆盖，CCCCC");
 	}
 	
 	public void methodB() {
