c76a0d02cb386c128c7429f059c958818e6c0bbd
travelerlife <travelerlife@users.noreply.github.com>
2021-05-19 01:20:47 +0800

    A修改，111，直接重新编辑本次提交内容（包括说明和文件）。

--- src/main/java/com/dhw/test/git/ForRollback.java
+++ src/main/java/com/dhw/test/git/ForRollback.java
@@ -14,5 +14,6 @@ public class ForRollback {
 	public static void main(String[] args) {
 		System.out.println("A本地reset到“A提交1版本，稍微修改（必须）并提交推送（必须强制覆盖，否则又的先pull最新版再push）为最新版。”");
 		System.out.println("A修改，直接创建并提交到远程的一个新的tag");
+		System.out.println("A修改，111，直接重新编辑本次提交内容（包括说明和文件）。");
 	}
 }
